cf659fc620562f8796c048283f7333d1905d6fad
Roberto Tienda <rbirt.datahelix@gmail.com>
2013-02-26 10:53:29 -0600

    Agregado función de likes

--- CalendarioUTVApp/Scripts/setlikes.js
+++ CalendarioUTVApp/Scripts/setlikes.js
@@ -1,29 +1,29 @@
 var evtid;
 
-function setLikes(id, listid) {
+function setLikes(contexto, id, listid) {
 
-	var reputation = Microsoft.Office.Server.ReputationModel.Reputation;
+    var reputation = Microsoft.Office.Server.ReputationModel.Reputation;
 
-	//reputation.setLike(context, '{57e87912-1b19-4a4d-8af3-2531014dca23}', id, true);
-	reputation.setLike(context, listid, id, true);
-	
-	context.executeQueryAsync(successSetLikes, errorSetLikes);
+    //reputation.setLike(context, '{57e87912-1b19-4a4d-8af3-2531014dca23}', id, true);
+    reputation.setLike(contexto, listid, id, true);
+
+    contexto.executeQueryAsync(successSetLikes, errorSetLikes);
 }
 
-function successSetLikes(){
+function successSetLikes() {
 }
 
 function errorSetLikes(sender, args) {
-	alert('Request failed. ' + args.get_message());
+    alert('Request failed. ' + args.get_message());
 }
 
-function setUnlikes(id, listid) {
-	var reputation = Microsoft.Office.Server.ReputationModel.Reputation;
+function setUnlikes(contexto, id, listid) {
+    var reputation = Microsoft.Office.Server.ReputationModel.Reputation;
 
-	//reputation.setLike(context, '{57e87912-1b19-4a4d-8af3-2531014dca23}', id, false);
-	reputation.setLike(context, listid, id, false);
+    //reputation.setLike(context, '{57e87912-1b19-4a4d-8af3-2531014dca23}', id, false);
+    reputation.setLike(contexto, listid, id, false);
 
-	context.executeQueryAsync(successSetUnlikes, errorSetUnlikes);
+    contexto.executeQueryAsync(successSetUnlikes, errorSetUnlikes);
 }
 
 function successSetUnlikes() {
@@ -31,5 +31,5 @@ function successSetUnlikes() {
 }
 
 function errorSetUnlikes(sender, args) {
-	alert('Request failed. ' + args.get_message());
+    alert('Request failed. ' + args.get_message());
 }
